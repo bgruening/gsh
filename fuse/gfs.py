46a1b37adf27fceef1eb5f908ff86b5e606050c7
Eric Rasche <rasche.eric@gmail.com>
2015-10-14 10:12:44 -0500

    autopep8 from editor for whitespace

--- fuse/gfs.py
+++ fuse/gfs.py
@@ -19,7 +19,7 @@ class GFSObject(Operations):
 
 class GFSManager(Operations):
     context = None #should be abstract to force sub-class implementation
-    
+
     def delegate(self, op, *args):
 
         boundObj = self._path_bound(args[0])
@@ -27,46 +27,46 @@ class GFSManager(Operations):
 
         if not hasattr(boundObj, op):
             raise FuseOSError(EFAULT)
-            
-        return getattr(boundObj, op)(*args)  
+
+        return getattr(boundObj, op)(*args)
 
 class GalaxyFS(Operations):
-    
+
     def __init__(self, galaxy_url, api_key):
         self.gi = objects.GalaxyInstance(url=galaxy_url, api_key=api_key)
         self.root = RootDirectory()
-        
+
         #TODO: for all subclasses of GFSObject register path, module grab classes inspect inheritance, b00m
         self.path_bindings = {
                                 RootDirectory.context : self.root,
                                 HistoryManager.context : HistoryManager(self),
                              }
-    
+
     def _path_bound(self, path):
         pbits = path.split(os.path.sep)
-        
+
         if len(pbits) == 1: # ['']/
             bind = self.path_bindings.get(path)
         elif len(pbits) > 1: # e.g. ['']/['histories']/['Unnamed History [8997977]']
-            bind = self.path_bindings.get(pbits[1], False)       
+            bind = self.path_bindings.get(pbits[1], False)
             if not bind: # i.e. top level directory couldn't match
                 bind = self.root # delegate handling to root directory
-                
+
         return bind
-    
+
     def __call__(self, op, *args):
         print '{} -> {} ::'.format(op, args[0]),
         boundObj = self._path_bound(args[0])
         return boundObj.delegate(op, *args)
 
 class Directory(): #should subclass file? or fusepy object?
-    
+
     def readdir(self, path=None, fh=None):
         return ['.','..']
-    
+
     def getattr(self, path=None, fh=None):
 
-        st = dict(st_mode=(S_IFDIR | 0700), st_nlink=2) #TODO: nlinks            
+        st = dict(st_mode=(S_IFDIR | 0700), st_nlink=2) #TODO: nlinks
         # do we want actual create times?
         st['st_ctime'] = st['st_mtime'] = st['st_atime'] = time.time()
         st['st_uid'] = os.geteuid()
@@ -74,9 +74,9 @@ class Directory(): #should subclass file? or fusepy object?
         return st
 
 class File():
-    
+
     def getattr(self, path=None, fh=None):
-        st = dict(st_mode=(S_IFREG | 0400), st_nlink=2) #TODO: nlinks            
+        st = dict(st_mode=(S_IFREG | 0400), st_nlink=2) #TODO: nlinks
         # do we want actual create times?
         st['st_ctime'] = st['st_mtime'] = st['st_atime'] = time.time()
         st['st_uid'] = os.geteuid()
@@ -86,14 +86,14 @@ class File():
 class RootDirectory(Directory, GFSManager):
     context = '/'
     tlds = ['histories']#, 'libraries', 'tools', 'workflows']
-    
+
     def _path_bound(self, path):
         return self
-    
+
     def getattr(self, path=None, fh=None):
-        
+
         if path == '/':
-            st = dict(st_mode=(S_IFDIR | 0500), st_nlink=2) #TODO: nlinks            
+            st = dict(st_mode=(S_IFDIR | 0500), st_nlink=2) #TODO: nlinks
             # do we want actual create times?
             st['st_ctime'] = st['st_mtime'] = st['st_atime'] = time.time()
             st['st_uid'] = os.geteuid()
@@ -101,17 +101,17 @@ class RootDirectory(Directory, GFSManager):
             return st
         else:
             raise FuseOSError(ENOENT)
-            
+
     def readdir(self, path=None, fh=None):
         return RootDirectory.tlds+super(RootDirectory, self).readdir()
 
 class HistoryManager(GFSObject, GFSManager):
     context = 'histories'
-    
+
     def __init__(self, gfs):
         super(HistoryManager, self).__init__(gfs)
         self.transactionMap = {}
-    
+
     def _path_bound(self, path):
         if path in self.transactionMap:
             wd = self.transactionMap.get(path).split(os.path.sep)
@@ -132,36 +132,36 @@ class HistoryManager(GFSObject, GFSManager):
             return History(hist_id, self.gfs).getDataset(dataset_id)
 
 class Histories(Directory, GFSObject):
-    
+
     def __init__(self, manager):
         self.manager = manager
         super(Histories, self).__init__(manager.gfs)
-    
+
     def getattr(self, path=None, fh=None):
         if path == '/histories':
             return super(Histories, self).getattr(path, fh)
         else:
             raise FuseOSError(ENOENT)
-            
+
     def readdir(self, path=None, fh=None):
         return [x.name+' ['+x.id+']' for x in self.gfs.gi.histories.list()]+super(Histories, self).readdir()
-    
+
     def mkdir(self, path=None, mode=None):
         new_hist = self.gfs.gi.histories.create(path[path.rfind(os.path.sep)+1:])
         self.manager.transactionMap[path] = '/histories/{} [{}]'.format(new_hist.name, new_hist.id)
-    
+
 class History(Directory, GFSObject):
 
     def __init__(self, hist_id, gfs):
         super(History, self).__init__(gfs)
         self.hist = self.gfs.gi.histories.get(hist_id)
-        
+
     def rmdir(self, path):
         self.gfs.gi.histories.delete(self.hist.id)
 
     def rename(self, old, new):
         path = new[:new.rfind(os.path.sep)]
-        
+
         if path != '/histories':
             raise FuseOSError(EPERM)
         name = new[new.rfind(os.path.sep)+1:]
@@ -169,16 +169,16 @@ class History(Directory, GFSObject):
 
     def readdir(self, path=None, fh=None):
         return ['{}. {} [{}]'.format(x.wrapped['hid'], x.name, x.id) for x in self.hist.content_infos if not x.deleted]+super(History, self).readdir()
-    
+
     def getDataset(self, dataset_id):
         return HistoryDataset(self.hist, dataset_id, self.gfs)
-    
+
 class HistoryDataset(File, GFSObject):
-    
+
     def __init__(self, hist, dataset_id, gfs):
         super(HistoryDataset, self).__init__(gfs)
         self.dataset = hist.get_dataset(dataset_id)
-        
+
     def unlink(self, path):
         self.dataset.delete()
 
@@ -189,7 +189,7 @@ if __name__ == '__main__':
     parser.add_argument('api_key', help='API key for Galaxy user')
     parser.add_argument('-m', '--mountpoint', default='gfs', help='Mount bound Galaxy instance here')
     args = parser.parse_args()
-    
+
     if not os.path.exists(args.mountpoint):
         os.makedirs(args.mountpoint)
 
